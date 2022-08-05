b30efc10eca1deb4e7a002cd2ff7d8580df372ef
Brandt Lareau <blar@hey.com>
2022-08-05 11:09:09 -0700

    Create a system to update workflow

---
+++ lib/update/latest_release.rb
@@ -0,0 +1,24 @@
+module Update
+  class LatestRelease
+    RELEASES = URI('https://api.github.com/repos/brand-it/alfred-workflow-ruby-and-rails-docs/releases')
+
+    Response = Struct.new(:response, :browser_download_uri)
+
+    def call
+      Response.new(response, browser_download_uri)
+    end
+
+    def browser_download_uri
+      url = response&.dig('assets')&.find do |a|
+        a['name'] == 'Ruby.Rails.API.Docs.alfredworkflow'
+      end&.dig('browser_download_url')
+      URI(url) if url
+    end
+
+    def response
+      @response ||= JSON.parse(Net::HTTP.get(RELEASES)).first || {}
+    rescue JSON::ParserError
+      {}
+    end
+  end
+end
